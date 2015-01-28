f73ebdeb2b49b3ab7194473a45e993a1519d4b0d
POSTRS01 <POSTRS01@BRNOLPT-DEV29.DSG.DSGROOT.INT>
2015-01-28 11:28:52 +0100

    Separate construct methods

--- galleryJSON.class.php
+++ galleryJSON.class.php
@@ -21,252 +21,13 @@
  * @Support    https://github.com/Kris-B/galleryJSON/issues
  *
  */
-
-require 'galleryJSON.Encoding.php';
+require './nanoPhotosProvider.Encoding.php';
 
 class galleryJSON
 {
 
     public function __construct()
     {
-
-        // RETRIEVE THE COVER IMAGE (THUMBNAIL) OF ONE ALBUM (FOLDER)
-        function GetAlbumCover($baseFolder)
-        {
-
-            // look for cover image
-            $files = glob($baseFolder . '/' . $GLOBALS['albumCoverDetector'] . '*.*');
-            if (count($files) > 0) {
-                $i = basename($files[0]);
-                if (preg_match("/\.(" . $GLOBALS['fileExtensions'] . ")*$/i", $i)) {
-                    $tn = GetThumbnail($baseFolder, $i);
-                    if ($tn != '') {
-                        return $tn;
-                    }
-                }
-            }
-
-            // no cover image found --> use the first image for the cover
-            $i = GetFirstImageFolder($baseFolder);
-            if ($i != '') {
-                $tn = GetThumbnail($baseFolder, $i);
-                if ($tn != '') {
-                    return $tn;
-                }
-            }
-
-            return '';
-        }
-
-        // Retrieve the first image of one folder --> ALBUM THUMBNAIL
-        function GetFirstImageFolder($folder)
-        {
-            $image = '';
-
-            $dh       = opendir($folder);
-            while (false !== ($filename = readdir($dh))) {
-                if (is_file($folder . '/' . $filename) && preg_match("/\.(" . $GLOBALS['fileExtensions'] . ")*$/i", $filename)) {
-                    $image = $filename;
-                    break;
-                }
-            }
-            closedir($dh);
-
-            return $image;
-        }
-
-        // RETRIEVE ONE IMAGE'S THUMBNAIL
-        function GetThumbnail($baseFolder, $filename)
-        {
-            $tn = $baseFolder . '_thumbnails/' . $filename;
-
-            if (file_exists($tn)) {
-                //if( filemtime($tn) < filemtime($baseFolder.$filename) ) {
-                // image file is older as the thumbnail file
-                return '_thumbnails/' . $filename;
-                //}
-            }
-
-            // generate the thumbnail
-            $tn = GenerateThumbnail($baseFolder, $filename);
-            if ($tn != '') {
-                return $tn;
-            }
-
-            // fallback: original image (no thumbnail)
-            return $filename;
-        }
-
-        // GENERATE ONE THUMBNAIL
-        function GenerateThumbnail($baseFolder, $filename)
-        {
-            if (!$GLOBALS['thumbnailsGenerate']) {
-                return '';
-            }
-
-            $td = $baseFolder . '/_thumbnails';
-            if (!file_exists($td)) {
-                mkdir($td, 0777, true);
-            }
-            //$orgImage = imagecreatefromjpeg($baseFolder.'/'.$filename);
-
-            $size = getimagesize($baseFolder . '/' . $filename);
-            switch ($size['mime']) {
-                case 'image/jpeg':
-                    $orgImage = imagecreatefromjpeg($baseFolder . '/' . $filename);
-                    break;
-                case 'image/gif':
-                    $orgImage = imagecreatefromgif($baseFolder . '/' . $filename);
-                    break;
-                case 'image/png':
-                    $orgImage = imagecreatefrompng($baseFolder . '/' . $filename);
-                    break;
-                default:
-                    return '';
-                    break;
-            }
-            $width  = $size[0];
-            $height = $size[1];
-
-            $tnFilename  = $baseFolder . '/_thumbnails/' . $filename;
-            $thumbWidth  = $GLOBALS['thumbnailSizes']['width'];
-            $thumbHeight = $GLOBALS['thumbnailSizes']['height'];
-
-            $originalAspect = $width / $height;
-            $thumbAspect    = $thumbWidth / $thumbHeight;
-
-            if ($GLOBALS['thumbnailSizes']['crop']) {
-                // CROP THE IMAGE
-                // some inspiration found in donkeyGallery (from Gix075) https://github.com/Gix075/donkeyGallery 
-                if ($originalAspect >= $thumbAspect) {
-                    // If image is wider than thumbnail (in aspect ratio sense)
-                    $newHeight = $thumbHeight;
-                    $newWidth  = $width / ($height / $thumbHeight);
-                } else {
-                    // If the thumbnail is wider than the image
-                    $newWidth  = $thumbWidth;
-                    $newHeight = $height / ($width / $thumbWidth);
-                }
-
-                $thumb = imagecreatetruecolor($thumbWidth, $thumbHeight);
-
-                // Resize and crop
-                imagecopyresampled($thumb, $orgImage, 0 - ($newWidth - $thumbWidth) / 2, // dest_x: Center the image horizontally
-                        0 - ($newHeight - $thumbHeight) / 2, // dest-y: Center the image vertically
-                        0, 0, // src_x, src_y
-                        $newWidth, $newHeight, $width, $height);
-            } else {
-                // DO NOT CROP
-                if ($originalAspect >= $thumbAspect) {
-                    $newHeight = $height / $width * $thumbWidth;
-                    $newWidth  = $thumbWidth;
-                } else {
-                    $newWidth  = $width / $height * $thumbHeight;
-                    $newHeight = $thumbHeight;
-                }
-
-                $thumb = imagecreatetruecolor($newWidth, $newHeight);
-
-                // Resize
-                imagecopyresampled($thumb, $orgImage, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
-            }
-
-            switch ($size['mime']) {
-                case 'image/jpeg':
-                    imagejpeg($thumb, $tnFilename, 90);
-                    break;
-                case 'image/gif':
-                    imagegif($thumb, $tnFilename);
-                    break;
-                case 'image/png':
-                    imagepng($thumb, $tnFilename, 1);
-                    break;
-            }
-
-            return '_thumbnails/' . $filename;
-        }
-
-        // Extract title and description from filename
-        function GetTitleDesc($filename, $isImage)
-        {
-            if ($isImage) {
-                $filename = file_ext_strip($filename);
-            }
-
-            $oneItem = new item();
-            if (strpos($filename, $GLOBALS['titleDescSeparator']) > 0) {
-                // title and description
-                $s              = explode($GLOBALS['titleDescSeparator'], $filename);
-                $oneItem->title = CustomEncode($s[0]);
-                if ($isImage) {
-                    $oneItem->description = CustomEncode(preg_replace('/.[^.]*$/', '', $s[1]));
-                } else {
-                    $oneItem->description = CustomEncode($s[1]);
-                }
-            } else {
-                // only title
-                if ($isImage) {
-                    $oneItem->title = CustomEncode($filename);  //(preg_replace('/.[^.]*$/', '', $filename));
-                } else {
-                    $oneItem->title = CustomEncode($filename);
-                }
-                $oneItem->description = '';
-            }
-
-            $oneItem->title = str_replace($GLOBALS['albumCoverDetector'], '', $oneItem->title);   // filter cover detector string
-            return $oneItem;
-        }
-
-        // Returns only the file extension (without the period).
-        function file_ext($filename)
-        {
-            if (!preg_match('/./', $filename))
-                return '';
-            return preg_replace('/^.*./', '', $filename);
-        }
-
-        // Returns the file name, less the extension.
-        function file_ext_strip($filename)
-        {
-            return preg_replace('/.[^.]*$/', '', $filename);
-        }
-
-        function CustomEncode($s)
-        {
-            return \ForceUTF8\Encoding::toUTF8(($s));
-            //return \ForceUTF8\Encoding::fixUTF8(($s));
-        }
-
-        function CustomDecode($s)
-        {
-            return utf8_decode($s);
-            // return $s;
-        }
-
-        function compare($a, $b)
-        {
-            $al = strtolower($a->title);
-            $bl = strtolower($b->title);
-            if ($al == $bl) {
-                return 0;
-            }
-            $b = false;
-            switch ($GLOBALS['sortOrder']) {
-                case 'DESC' :
-                    if ($al < $bl) {
-                        $b = true;
-                    }
-                    break;
-                case 'ASC':
-                default:
-                    if ($al > $bl) {
-                        $b = true;
-                    }
-                    break;
-            }
-            return ($b) ? +1 : -1;
-        }
-
         // retrieve the album ID in the URL
         $album   = '/';
         $albumID = '';
@@ -384,6 +145,243 @@ class galleryJSON
             // return in JSON
             echo $output;
         }
+    }
+
+    // RETRIEVE THE COVER IMAGE (THUMBNAIL) OF ONE ALBUM (FOLDER)
+    function GetAlbumCover($baseFolder)
+    {
+
+        // look for cover image
+        $files = glob($baseFolder . '/' . $GLOBALS['albumCoverDetector'] . '*.*');
+        if (count($files) > 0) {
+            $i = basename($files[0]);
+            if (preg_match("/\.(" . $GLOBALS['fileExtensions'] . ")*$/i", $i)) {
+                $tn = GetThumbnail($baseFolder, $i);
+                if ($tn != '') {
+                    return $tn;
+                }
+            }
+        }
+
+        // no cover image found --> use the first image for the cover
+        $i = GetFirstImageFolder($baseFolder);
+        if ($i != '') {
+            $tn = GetThumbnail($baseFolder, $i);
+            if ($tn != '') {
+                return $tn;
+            }
+        }
+
+        return '';
+    }
+
+    // Retrieve the first image of one folder --> ALBUM THUMBNAIL
+    function GetFirstImageFolder($folder)
+    {
+        $image = '';
+
+        $dh       = opendir($folder);
+        while (false !== ($filename = readdir($dh))) {
+            if (is_file($folder . '/' . $filename) && preg_match("/\.(" . $GLOBALS['fileExtensions'] . ")*$/i", $filename)) {
+                $image = $filename;
+                break;
+            }
+        }
+        closedir($dh);
+
+        return $image;
+    }
+
+    function compare($a, $b)
+    {
+        $al = strtolower($a->title);
+        $bl = strtolower($b->title);
+        if ($al == $bl) {
+            return 0;
+        }
+        $b = false;
+        switch ($GLOBALS['sortOrder']) {
+            case 'DESC' :
+                if ($al < $bl) {
+                    $b = true;
+                }
+                break;
+            case 'ASC':
+            default:
+                if ($al > $bl) {
+                    $b = true;
+                }
+                break;
+        }
+        return ($b) ? +1 : -1;
+    }
+
+    // RETRIEVE ONE IMAGE'S THUMBNAIL
+    function GetThumbnail($baseFolder, $filename)
+    {
+        $tn = $baseFolder . '_thumbnails/' . $filename;
+
+        if (file_exists($tn)) {
+            //if( filemtime($tn) < filemtime($baseFolder.$filename) ) {
+            // image file is older as the thumbnail file
+            return '_thumbnails/' . $filename;
+            //}
+        }
+
+        // generate the thumbnail
+        $tn = GenerateThumbnail($baseFolder, $filename);
+        if ($tn != '') {
+            return $tn;
+        }
+
+        // fallback: original image (no thumbnail)
+        return $filename;
+    }
+
+    // GENERATE ONE THUMBNAIL
+    function GenerateThumbnail($baseFolder, $filename)
+    {
+        if (!$GLOBALS['thumbnailsGenerate']) {
+            return '';
+        }
+
+        $td = $baseFolder . '/_thumbnails';
+        if (!file_exists($td)) {
+            mkdir($td, 0777, true);
+        }
+        //$orgImage = imagecreatefromjpeg($baseFolder.'/'.$filename);
+
+        $size = getimagesize($baseFolder . '/' . $filename);
+        switch ($size['mime']) {
+            case 'image/jpeg':
+                $orgImage = imagecreatefromjpeg($baseFolder . '/' . $filename);
+                break;
+            case 'image/gif':
+                $orgImage = imagecreatefromgif($baseFolder . '/' . $filename);
+                break;
+            case 'image/png':
+                $orgImage = imagecreatefrompng($baseFolder . '/' . $filename);
+                break;
+            default:
+                return '';
+                break;
+        }
+        $width  = $size[0];
+        $height = $size[1];
+
+        $tnFilename  = $baseFolder . '/_thumbnails/' . $filename;
+        $thumbWidth  = $GLOBALS['thumbnailSizes']['width'];
+        $thumbHeight = $GLOBALS['thumbnailSizes']['height'];
+
+        $originalAspect = $width / $height;
+        $thumbAspect    = $thumbWidth / $thumbHeight;
+
+        if ($GLOBALS['thumbnailSizes']['crop']) {
+            // CROP THE IMAGE
+            // some inspiration found in donkeyGallery (from Gix075) https://github.com/Gix075/donkeyGallery 
+            if ($originalAspect >= $thumbAspect) {
+                // If image is wider than thumbnail (in aspect ratio sense)
+                $newHeight = $thumbHeight;
+                $newWidth  = $width / ($height / $thumbHeight);
+            } else {
+                // If the thumbnail is wider than the image
+                $newWidth  = $thumbWidth;
+                $newHeight = $height / ($width / $thumbWidth);
+            }
+
+            $thumb = imagecreatetruecolor($thumbWidth, $thumbHeight);
+
+            // Resize and crop
+            imagecopyresampled($thumb, $orgImage, 0 - ($newWidth - $thumbWidth) / 2, // dest_x: Center the image horizontally
+                    0 - ($newHeight - $thumbHeight) / 2, // dest-y: Center the image vertically
+                    0, 0, // src_x, src_y
+                    $newWidth, $newHeight, $width, $height);
+        } else {
+            // DO NOT CROP
+            if ($originalAspect >= $thumbAspect) {
+                $newHeight = $height / $width * $thumbWidth;
+                $newWidth  = $thumbWidth;
+            } else {
+                $newWidth  = $width / $height * $thumbHeight;
+                $newHeight = $thumbHeight;
+            }
+
+            $thumb = imagecreatetruecolor($newWidth, $newHeight);
+
+            // Resize
+            imagecopyresampled($thumb, $orgImage, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+        }
+
+        switch ($size['mime']) {
+            case 'image/jpeg':
+                imagejpeg($thumb, $tnFilename, 90);
+                break;
+            case 'image/gif':
+                imagegif($thumb, $tnFilename);
+                break;
+            case 'image/png':
+                imagepng($thumb, $tnFilename, 1);
+                break;
+        }
+
+        return '_thumbnails/' . $filename;
+    }
+
+    // Extract title and description from filename
+    function GetTitleDesc($filename, $isImage)
+    {
+        if ($isImage) {
+            $filename = file_ext_strip($filename);
+        }
+
+        $oneItem = new item();
+        if (strpos($filename, $GLOBALS['titleDescSeparator']) > 0) {
+            // title and description
+            $s              = explode($GLOBALS['titleDescSeparator'], $filename);
+            $oneItem->title = CustomEncode($s[0]);
+            if ($isImage) {
+                $oneItem->description = CustomEncode(preg_replace('/.[^.]*$/', '', $s[1]));
+            } else {
+                $oneItem->description = CustomEncode($s[1]);
+            }
+        } else {
+            // only title
+            if ($isImage) {
+                $oneItem->title = CustomEncode($filename);  //(preg_replace('/.[^.]*$/', '', $filename));
+            } else {
+                $oneItem->title = CustomEncode($filename);
+            }
+            $oneItem->description = '';
+        }
+
+        $oneItem->title = str_replace($GLOBALS['albumCoverDetector'], '', $oneItem->title);   // filter cover detector string
+        return $oneItem;
+    }
+
+// Returns only the file extension (without the period).
+    function file_ext($filename)
+    {
+        if (!preg_match('/./', $filename))
+            return '';
+        return preg_replace('/^.*./', '', $filename);
+    }
+
+    // Returns the file name, less the extension.
+    function file_ext_strip($filename)
+    {
+        return preg_replace('/.[^.]*$/', '', $filename);
+    }
+
+    function CustomEncode($s)
+    {
+        return \ForceUTF8\Encoding::toUTF8(($s));
+        //return \ForceUTF8\Encoding::fixUTF8(($s));
+    }
+
+    function CustomDecode($s)
+    {
+        return utf8_decode($s);
+        // return $s;
     }
 
 }
